26f08eb88a1feea0052ec353f26e9dbac2a05372
Rob Walker <rjwalker113@gmail.com>
2025-12-04 01:30:22 -0500

    ADDED: Universe() stores previous screens and can go back

--- state/universe.py
+++ state/universe.py
@@ -7,10 +7,12 @@ from companies.model import Company
 
 class Universe(EventDispatcher):
     active_screen = StringProperty("title")
+    previous_screen = StringProperty(None, allownone=True)
     company = ObjectProperty(None, allownone=True)
     roster = ListProperty([])
     titles = ListProperty([])
     events = ListProperty([])
+    active_gameplay_view = StringProperty("overview")
     current_match = ObjectProperty(None)
     match_dirty = BooleanProperty(False)
     booking_errors = ListProperty([])
@@ -33,3 +35,19 @@ class Universe(EventDispatcher):
         self.match_dirty = False
         self.booking_errors = []
         self.active_screen = "title"
+
+    def change_screen(self, screen):
+        '''Stores current active_screen as previous_screen, and then changes active_screen.'''
+        # copy the current value into previous_screen
+        self.previous_screen = self.active_screen
+        # now update active_screen
+        self.active_screen = screen
+        # Confirm
+        print(f"Moving from {self.previous_screen} to {self.active_screen}")
+
+    def change_gameplay_view(self, gameplay_view):
+        '''Changes current view in the gameplay screen.'''
+        # now update active_screen
+        self.active_gameplay_view = gameplay_view
+        # Confirm
+        print(f"Current Gameplay View set to: {self.active_gameplay_view}")
