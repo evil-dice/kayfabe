141c9f2e8b1a802055565125aef9e09597450807
Rob Walker <rjwalker113@gmail.com>
2026-04-04 21:46:55 -0400

    CHANGEl Updated containers to accept static sizes or calculate dynamic sizes if needed

--- core/widgets/containers.py
+++ core/widgets/containers.py
@@ -1,14 +1,28 @@
 from kivy.uix.boxlayout import BoxLayout
 
 class Row(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(orientation='horizontal', size_hint_y=None, **kwargs)
-        self.bind(minimum_height=self.setter('height'))  # height = sum of children
+    def __init__(self, size_hint_y=None, height=None, orientation="vertical", **kwargs):
+        super().__init__(orientation='horizontal', **kwargs)
+        self.size_hint_x = 1
+        self.size_hint_y = size_hint_y
+        self.spacing = 20
+        self.orientation = orientation
+        if height:
+            self.height = height
+        else:
+            self.bind(minimum_height=self.setter('height'))  # height = sum of children
 
 class Column(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(orientation='vertical', size_hint_x=None, **kwargs)
-        self.bind(minimum_width=self.setter('width'))  # width = sum of children
+    def __init__(self, size_hint_x=None, width=None, orientation='vertical', **kwargs):
+        super().__init__(**kwargs)
+        self.size_hint_x = size_hint_x
+        self.size_hint_y = 1
+        self.spacing = 20
+        self.orientation = orientation
+        if width:
+            self.width = width
+        else:
+            self.bind(minimum_width=self.setter('width'))  # width = sum of children
 
 class Box(BoxLayout):
     def __init__(self, width, height, **kwargs):
